d8339d38229ad951a30f91301b0c083bef3df42b
Kaydev-cmd <keamogetswingwako2001@gmail.com>
2024-11-03 07:36:48 -0800

    Added the learn more section, applied styling and responsiveness

---
+++ frontend/src/components/LearnMoreButton.jsx
@@ -0,0 +1,14 @@
+// import React from 'react'
+import "./LearnMoreButton.css";
+
+export const LearnMoreButton = () => {
+  return (
+    <>
+      <div className="learn-more-btn-wrapper">
+        <div className="learn-more-btn">
+          <button>Learn More</button>
+        </div>
+      </div>
+    </>
+  );
+};
