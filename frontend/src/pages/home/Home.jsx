d0cc131d2e16e71cd7cc027af3c69e672fbd8815
Kaydev-cmd <keamogetswingwako2001@gmail.com>
2024-11-06 15:28:23 -0800

    Added an interactive swiper

--- frontend/src/pages/home/Home.jsx
+++ frontend/src/pages/home/Home.jsx
@@ -1,64 +1,172 @@
 // import React from 'react'
 import "./Home.css";
 import { LearnMoreButton } from "../../components/LearnMoreButton";
+import { Swiper, SwiperSlide } from "swiper/react";
+
+// Import Required Modules
+import { Pagination, Autoplay } from "swiper/modules";
+
+// Import Swiper styles
+import "swiper/css";
+import "swiper/css/pagination";
 
 export const Home = () => {
   return (
     <>
       {/* Hero */}
-      {/* Ticket #1: Add interactive swiper */}
       {/* Ticket #2: Add interactive navigation for links across the page */}
       <section className="hero">
         <div className="container">
-          <div className="hero-content-wrapper">
-            <div className="hero-content">
-              <div className="hero-left-content-wrapper">
-                <div className="hero-left-content">
-                  <div className="hero-left-content-text-wrapper">
-                    <div className="hero-left-content-text">
-                      <h1>
-                        Lessons and insights
-                        <br />
-                        <span>
-                          <p>from 8 years</p>
-                        </span>
-                      </h1>
-                      <p>
-                        Where to grow your business as a photographer: site or
-                        social media?
-                      </p>
+          <Swiper
+            pagination={{
+              dynamicBullets: true,
+            }}
+            autoplay={{
+              delay: 12000,
+              disableOnInteraction: false,
+            }}
+            modules={[Pagination, Autoplay]}
+            className="hero-swiper"
+          >
+            <SwiperSlide>
+              <div className="hero-content-wrapper">
+                <div className="hero-content">
+                  <div className="hero-left-content-wrapper">
+                    <div className="hero-left-content">
+                      <div className="hero-left-content-text-wrapper">
+                        <div className="hero-left-content-text">
+                          <h1>
+                            Lessons and insights
+                            <br />
+                            <span>
+                              <p>from 8 years</p>
+                            </span>
+                          </h1>
+                          <p>
+                            Where to grow your business as a photographer: site
+                            or social media?
+                          </p>
+                        </div>
+                      </div>
+                      <div className="hero-left-content-btn-wrapper">
+                        <div className="hero-left-content-btn">
+                          <div className="register-btn-wrapper">
+                            <div className="register-btn">
+                              <button>Register</button>
+                            </div>
+                          </div>
+                        </div>
+                      </div>
                     </div>
                   </div>
-                  <div className="hero-left-content-btn-wrapper">
-                    <div className="hero-left-content-btn">
-                      <div className="register-btn-wrapper">
-                        <div className="register-btn">
-                          <button>Register</button>
+                  <div className="hero-right-content-wrapper">
+                    <div className="hero-right-content">
+                      <div className="hero-right-content-banner-wrapper">
+                        <div className="hero-right-content-banner">
+                          <img
+                            src="/images/home/Illustration.png"
+                            alt="Hero Illustration"
+                          />
                         </div>
                       </div>
                     </div>
                   </div>
                 </div>
               </div>
-              <div className="hero-right-content-wrapper">
-                <div className="hero-right-content">
-                  <div className="hero-right-content-banner-wrapper">
-                    <div className="hero-right-content-banner">
-                      <img
-                        src="/images/home/Illustration.png"
-                        alt="Hero Illustration"
-                      />
+            </SwiperSlide>
+            <SwiperSlide>
+              <div className="hero-content-wrapper">
+                <div className="hero-content">
+                  <div className="hero-left-content-wrapper">
+                    <div className="hero-left-content">
+                      <div className="hero-left-content-text-wrapper">
+                        <div className="hero-left-content-text">
+                          <h1>
+                            Lessons and insights
+                            <br />
+                            <span>
+                              <p>from 8 years</p>
+                            </span>
+                          </h1>
+                          <p>
+                            Where to grow your business as a photographer: site
+                            or social media?
+                          </p>
+                        </div>
+                      </div>
+                      <div className="hero-left-content-btn-wrapper">
+                        <div className="hero-left-content-btn">
+                          <div className="register-btn-wrapper">
+                            <div className="register-btn">
+                              <button>Register</button>
+                            </div>
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                  <div className="hero-right-content-wrapper">
+                    <div className="hero-right-content">
+                      <div className="hero-right-content-banner-wrapper">
+                        <div className="hero-right-content-banner">
+                          <img
+                            src="/images/home/Illustration.png"
+                            alt="Hero Illustration"
+                          />
+                        </div>
+                      </div>
                     </div>
                   </div>
                 </div>
               </div>
-            </div>
-          </div>
-          <div className="hero-content-swiper-icons-wrapper">
-            <div className="hero-content-swiper-icons">
-              <img src="/images/home/Dot.png" alt="Swiper Icons" />
-            </div>
-          </div>
+            </SwiperSlide>
+            <SwiperSlide>
+              <div className="hero-content-wrapper">
+                <div className="hero-content">
+                  <div className="hero-left-content-wrapper">
+                    <div className="hero-left-content">
+                      <div className="hero-left-content-text-wrapper">
+                        <div className="hero-left-content-text">
+                          <h1>
+                            Lessons and insights
+                            <br />
+                            <span>
+                              <p>from 8 years</p>
+                            </span>
+                          </h1>
+                          <p>
+                            Where to grow your business as a photographer: site
+                            or social media?
+                          </p>
+                        </div>
+                      </div>
+                      <div className="hero-left-content-btn-wrapper">
+                        <div className="hero-left-content-btn">
+                          <div className="register-btn-wrapper">
+                            <div className="register-btn">
+                              <button>Register</button>
+                            </div>
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                  <div className="hero-right-content-wrapper">
+                    <div className="hero-right-content">
+                      <div className="hero-right-content-banner-wrapper">
+                        <div className="hero-right-content-banner">
+                          <img
+                            src="/images/home/Illustration.png"
+                            alt="Hero Illustration"
+                          />
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </SwiperSlide>
+          </Swiper>
         </div>
       </section>
 
